e343d2fdb11830e06d4c9e291bae17d4618d0124
chanab0910 <chanab0910@highgateschool.org.uk>
2023-09-13 12:06:24 +0100

    queries stuff

--- Practicing_database_queries.py
+++ Practicing_database_queries.py
@@ -4,13 +4,18 @@ conn = sqlite3.connect("World_cup.sqlite3")
 cursor = conn.cursor()
 group1= []
 Create_tier_2 = """
-UPDATE Country
-SET Tier = 2
-WHERE Tier != 1;
-
+SELECT Country_name FROM Country
 """
 
 
 cursor.execute(Create_tier_2)
+for item in cursor.fetchall():
+    group1.append(item)
 conn.commit()
 conn.close()
+print(group1[0])
+
+
+class create_group:
+    def __init__(self):
+
