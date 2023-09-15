6eedeacd0ad2a63eef92a9622c50e7d33f536fb5
chanab0910 <chanab0910@highgateschool.org.uk>
2023-09-15 10:31:04 +0100

    changing to make LoL

--- create_groups.py
+++ create_groups.py
@@ -14,20 +14,21 @@ groups = []
 # create list of objects
 # don't have lots of variables with 1 parameter changed
 
+countries_in_tiers = []
 
-tier1 = sample(read_from_db.tier1_countries, len(read_from_db.tier1_countries))
-tier2 = sample(read_from_db.tier2_countries, len(read_from_db.tier2_countries))
-tier3 = sample(read_from_db.tier3_countries, len(read_from_db.tier3_countries))
-tier4 = sample(read_from_db.tier4_countries, len(read_from_db.tier4_countries))
+countries_in_tiers.append(sample(read_from_db.tier1_countries, len(read_from_db.tier1_countries)))
+countries_in_tiers.append(sample(read_from_db.tier2_countries, len(read_from_db.tier2_countries)))
+countries_in_tiers.append(sample(read_from_db.tier3_countries, len(read_from_db.tier3_countries)))
+countries_in_tiers.append(sample(read_from_db.tier4_countries, len(read_from_db.tier4_countries)))
 
-for group, country in zip([group8, group7, group6, group5, group4, group3, group2, group1], tier1):
-    group.append(country)
+#for group, country in zip([group8, group7, group6, group5, group4, group3, group2, group1], tier1):
+#    group.append(country)
 
-for group, country in zip([group8, group7, group6, group5, group4, group3, group2, group1], tier2):
-    group.append(country)
+#for group, country in zip([group8, group7, group6, group5, group4, group3, group2, group1], tier2):
+ #   group.append(country)
 
-for group, country in zip([group8, group7, group6, group5, group4, group3, group2, group1], tier3):
-    group.append(country)
+#for group, country in zip([group8, group7, group6, group5, group4, group3, group2, group1], tier3):
+ #   group.append(country)
 
-for group, country in zip([group8, group7, group6, group5, group4, group3, group2, group1], tier4):
-    group.append(country)
+#for group, country in zip([group8, group7, group6, group5, group4, group3, group2, group1], tier4):
+ #   group.append(country)
