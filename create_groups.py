7fbc07775f19edbffc360fdf6cf0c66c40c6dae8
chanab0910 <chanab0910@highgateschool.org.uk>
2023-09-27 13:13:24 +0100

    Experimenting with the DB

--- create_groups.py
+++ create_groups.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 
-class Group_generator:
+class GroupGenerator:
     def __init__(self):
         self.team = ''
         self.groups = [[] for i in range(8)]
@@ -15,29 +15,28 @@ class Group_generator:
         self.countries_in_tiers.append(sample(read_from_db.tier4_countries, len(read_from_db.tier4_countries)))
 
     def group_draw(self):
-        for i,group in enumerate(self.groups):
+        group = []
+        for i, group in enumerate(self.groups):
             self.team = self.countries_in_tiers[i][randint(0, len(self.countries_in_tiers[i]))]
             group.append(self.team)
             self.countries_in_tiers[i].remove(self.team)
         return group
 
-class Simulate_groups:
+
+class SimulateGroups:
     def __init__(self):
         self.Home_team_score = None
         self.base = 0.0128125
-        self.groups = Group_generator.group_draw()
+        self.groups = GroupGenerator.group_draw()
         self.engine = self.create_engine('sqlite:///World_cup.sqlite3', echo=True)
 
         self.sess = self.Session(self.engine)
 
     def sim_match(self):
         for group in self.groups:
-            if self.Calculate_goals(self.sess.query(self.Country.Attack).filter_by(self.Country.Country_name = group[0]))
-    def Calculate_goals(self):
+            if self.calculate_goals(
+                    self.sess.query(self.Country.Attack).filter_by(self.Country.Country_name == group[0])):
                 ...
 
-
-
-
-
-
+    def calculate_goals(self):
+        ...
